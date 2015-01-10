a8b3a075ffd964a0e8db70c49ec959cbefc76090
erikcas <erikcas1972@gmail.com>
2015-01-10 12:05:19 +0100

    [test] build regardless all
    Change-Id: I509c1bfe0fd9d8bf31f228ebef4fc5b5184832b4

--- Android.mk
+++ Android.mk
@@ -1,7 +1,2 @@
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(BOARD_VENDOR),sony)
-ifeq ($(call is-board-platform-in-list,msm8974 msm8960 msm8660 msm8226),true)
-    include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
-endif
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
